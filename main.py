5fa1768eec12e3937567376d9a3dd08882398f6c
Matthew Sharp <71024145+MatthewSharpOTU@users.noreply.github.com>
2022-01-22 20:11:30 +0000

    Begin implementation of "Collectibles" class

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ import json
 import time
 import logging
 import threading 
+from replit import db
+
 from keep_alive import keep_alive
 
 client = discord.Client()
+
+class Collectibles:
+  __agents = {}
+
+  def __init__(self):
+    self.__agents={"Astra":False, "Breach":False, "Brimstone":False, "Chamber":False, "Cypher":False,"Jett":False, "KAY/O":False, "Killjoy":False, "Neon":False, "Omen":False, "Phoenix":False, "Raze":False, "Reyna":False, "Sage":False, "Skye":False, "Sova":False,  "Viper":False, "Yoru":False}
 
 
 @client.event
@@ -22,6 +30,25 @@ async def on_message(message):
     return
   if message.content.startswith('$hello'):
     await message.channel.send('Hello {}'.format(message.author.name))
+  if message.content.startswith('$startnew'):
+    new_user = False
+    for k in db.keys():
+      if str(message.author) == k:
+        new_user = True
+        break
+      
+    if new_user == True:
+      await message.channel.send('This user has an account associated with MEV Bot')
+    else:
+      db[message.author] = Collectibles()
+
+      await message.channel.send('Welcome to MEV Bot, this is a bot that will allow you to collect different valorant abilities, charms, skins and agents. Since this is your first time you have one free lootbox for an agent and their abilities, 2 charms and a skin. Take part in the casino, complete daily and weekly tasks, random pop questions, and many more to earn more currency to collect them all. If you roll a duplicate use $claim to earn currency. Good luck and enjoy MEV Bot')
+  
+  if message.content.startswith('$reset'):
+    del db[str(message.author)]
+    print(db.keys())
+  
+  
 
 keep_alive()
 my_secret = os.environ['TOKEN']
