d5bd26142b1ddce0bbb8f972bd2ca3f56628b498
msharp-tc <msharp@tucowsinc.com>
2022-01-26 15:56:31 -0500

    Updated $sussy command

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 import re
 import asyncio
 import json
 import time
 import logging
 import threading 
-from replit import db
-from keep_alive import keep_alive
+# from keep_alive import keep_alive
 
 
 try:
@@ -16,7 +17,7 @@ except:
     os.system("pip install git+https://github.com/Merubokkusu/Discord-S.C.U.M.git#egg=discum")
     import discum
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!", case_insensitive=True)
 
 class Collectibles():
   agents = {}
@@ -28,6 +29,7 @@ class Collectibles():
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+  return await client.change_presence(activity=discord.Activity(type=1, name='Vibing'))
 
 @client.event
 async def on_message(message):
@@ -36,26 +38,12 @@ async def on_message(message):
   if message.content.startswith('$hello'):
     await message.channel.send('```Hello {}```'.format(message.author.name))
   if message.content.startswith('$sussy'):
-    await message.channel.send("<:sussy:935624898511011930>")
+    await message.channel.send("<:sussy:935624408855363666>")
   if message.content.startswith('$startnew'):
-    new_user = False
-    for k in db.keys():
-      if str(message.author) == k:
-        new_user = True
-        break
-      
-    if new_user == True:
-      await message.channel.send('```This user has an account associated with MEV Bot```')
-    else:
-      user = Collectibles()
-      print(user.agents["Astra"])
-      db[message.author] = {"Astra":False, "Breach":False, "Brimstone":False, "Chamber":False, "Cypher":False,"Jett":False, "KAY/O":False, "Killjoy":False, "Neon":False, "Omen":False, "Phoenix":False, "Raze":False, "Reyna":False, "Sage":False, "Skye":False, "Sova":False,  "Viper":False, "Yoru":False}
-
-      await message.channel.send('```Welcome to MEV Bot, this is a bot that will allow you to collect different valorant abilities, charms, skins and agents. Since this is your first time you have one free lootbox for an agent and their abilities, 2 charms and a skin. Take part in the casino, complete daily and weekly tasks, random pop questions, and many more to earn more currency to collect them all. If you roll a duplicate use $claim to earn currency. Good luck and enjoy MEV Bot```')
+    await message.channel.send('```Welcome to MEV Bot, this is a bot that will allow you to collect different valorant abilities, charms, skins and agents. Since this is your first time you have one free lootbox for an agent and their abilities, 2 charms and a skin. Take part in the casino, complete daily and weekly tasks, random pop questions, and many more to earn more currency to collect them all. If you roll a duplicate use $claim to earn currency. Good luck and enjoy MEV Bot```')
   
   if message.content.startswith('$reset'):
-    del db[str(message.author)]
-    print(db.keys())
+    return 
 
   if message.content.startswith('$embed'):
     embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
@@ -64,8 +52,8 @@ async def on_message(message):
     embedVar.set_image(url="https://www.looper.com/img/gallery/valorant-release-date-platforms-trailer-and-gameplay/intro-1591383833.jpg")
     await message.channel.send(embed=embedVar)
   
-  
+load_dotenv()
 
-keep_alive()
-my_secret = os.environ['TOKEN']
+# keep_alive()
+my_secret = os.getenv('TOKEN')
 client.run(my_secret)
