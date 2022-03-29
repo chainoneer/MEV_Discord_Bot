aa01d139ec1f05cea53a21d65351cd5845e348d4
Matthew Sharp <71024145+MatthewSharpOTU@users.noreply.github.com>
2022-03-29 19:36:28 +0000

    Implemented Randomizer for Agents with corresponding agent profiles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import discord
 import re
 import asyncio
@@ -18,12 +19,11 @@ except:
 
 client = discord.Client()
 
-class Collectibles():
-  agents = {}
+agents = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Jett", "KAY/O",
+          "Killjoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye",
+          "Sova", "Viper", "Yoru"]
 
-  def __init__(self):
-    self.agents={"Astra":False, "Breach":False, "Brimstone":False, "Chamber":False, "Cypher":False,"Jett":False, "KAY/O":False, "Killjoy":False, "Neon":False, "Omen":False, "Phoenix":False, "Raze":False, "Reyna":False, "Sage":False, "Skye":False, "Sova":False,  "Viper":False, "Yoru":False}
-
+emotes = ["<:astra:958420104490663986>","<:breach:958420500839804938>","<:brimstone:958420774736252948>","<:chamber:958421177422983239>","<:cypher:958425821578682378>","<:jett:958426375168073809>","<:kayo:958426726822727711>","<:killjoy:958427157539983440>","<:neon:958427417528131624>","<:omen:958427990029660230>","<:phoenix:958428146330374164>","<:raze:958428251485782046>","<:reyna:958428346428059648>","<:sage:958428493211902003>","<:skye:958428627517730876>","<:sova:958429598989811812>","<:viper:958429995989086238>","<:yoru:958430128977895504>"]
 
 @client.event
 async def on_ready():
@@ -35,8 +35,9 @@ async def on_message(message):
     return
   if message.content.startswith('$hello'):
     await message.channel.send('```Hello {}```'.format(message.author.name))
-  if message.content.startswith('$sussy'):
-    await message.channel.send("<:sussy:935624898511011930>")
+  if message.content.startswith('$emote'):
+    await message.channel.send("<:sussy:935624408855363666>")
+
   if message.content.startswith('$startnew'):
     new_user = False
     for k in db.keys():
@@ -47,16 +48,36 @@ async def on_message(message):
     if new_user == True:
       await message.channel.send('```This user has an account associated with MEV Bot```')
     else:
-      user = Collectibles()
-      print(user.agents["Astra"])
-      db[message.author] = {"Astra":False, "Breach":False, "Brimstone":False, "Chamber":False, "Cypher":False,"Jett":False, "KAY/O":False, "Killjoy":False, "Neon":False, "Omen":False, "Phoenix":False, "Raze":False, "Reyna":False, "Sage":False, "Skye":False, "Sova":False,  "Viper":False, "Yoru":False}
-
       await message.channel.send('```Welcome to MEV Bot, this is a bot that will allow you to collect different valorant abilities, charms, skins and agents. Since this is your first time you have one free lootbox for an agent and their abilities, 2 charms and a skin. Take part in the casino, complete daily and weekly tasks, random pop questions, and many more to earn more currency to collect them all. If you roll a duplicate use $claim to earn currency. Good luck and enjoy MEV Bot```')
   
   if message.content.startswith('$reset'):
     del db[str(message.author)]
     print(db.keys())
 
+  if message.content.startswith('$randomval'):
+    l = len(message.content)
+    if l>10:
+      if message.content[11] == "c":
+        print("Custom Game")
+        attside = int(message.content[13])
+        defside = int(message.content[15]) 
+        attackers = []
+        emotesatt = []
+        defenders = []
+        emotesdef = []
+        for i in range(attside):
+          attackers.append(agents[random.randint(0,17)])
+        for i in range(defside):
+          defenders.append(agents[random.randint(0,17)])
+        response = "Attackers: "+ attackers[0] + 
+        await message.channel.send(response)
+        
+      else:
+        print("Error")
+    else:
+      print("Random Agent")
+    await message.channel.send('```Hello {}```'.format(message.author.name))
+  
   if message.content.startswith('$embed'):
     embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
     embedVar.add_field(name="Field1", value="hi", inline=False)
@@ -64,8 +85,6 @@ async def on_message(message):
     embedVar.set_image(url="https://www.looper.com/img/gallery/valorant-release-date-platforms-trailer-and-gameplay/intro-1591383833.jpg")
     await message.channel.send(embed=embedVar)
   
-  
-
 keep_alive()
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
